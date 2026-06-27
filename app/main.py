3fa8468c49c4f9280cfd27d0d433f9dba2698f23
Gleyser Bomfim Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2026-06-27 13:20:01 -0300

    Inserindo a descricao da API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="API Adoção de Animais",
+    version="1.0.0",
+    description="API REST para gerenciamento de animais disponíveis para adoção."
+)
 
 @app.get("/")
 def inicio():
